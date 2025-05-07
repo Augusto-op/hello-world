e9d850bb53e78866ac91c7f14c1cafc729a5492b
Augusto-op <augustoramossilva444@gmail.com>
2025-05-07 18:50:44 +0000

    Revisao  de soma

--- Revisao.py
+++ Revisao.py
@@ -24,3 +24,15 @@ if a > b:
     print("A e MAIOR que B")
 else:
    print("A e menor que B")
+
+#explo de soma
+soma = (1 + 1)
+print(soma)
+
+#exemplos de subtracão
+subtracao = (10 - 5)
+print(subtracao)
+
+#exemplo multiplicacão
+multiplicacao = (2 * 4)
+print("A multiplicacao de 2 * 4 é:",multiplicacao)
