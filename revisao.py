65f733276bb23f0bb23d02abaef1028dec424d9a
Augusto-op <augustoramossilva444@gmail.com>
2025-05-07 15:17:13 -0300

    revisão da materia passada

---
+++ revisao.py
@@ -0,0 +1,18 @@
+ #comentario
+
+ #variavel e um espaço na memorí que receberá dado
+ #tipos : texto, número (int e float(moeda,altura,peso))
+
+ #exemplo de número float (flutuante)
+ peso = 89.5
+ print(peso)
+
+ #exemplos de números inteiros 
+ idade = 18
+ print(idade)
+
+ #exemplos de variavel do tipo de texto
+ nome = ("maria")
+ sobrenome = "silva"
+ print(nome,sobrenome)
+ 
