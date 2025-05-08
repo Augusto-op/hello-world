f4f3a9fc5a4f4f8539050ad0991059383d71d0e2
Augusto-op <augustoramossilva444@gmail.com>
2025-05-08 18:00:11 +0000

    ecercicio de media

---
+++ Exercicio.py
@@ -0,0 +1,11 @@
+nota1 = int(input("insira as primeira notas:"))
+#print(numero1)
+
+nota2 = float(input("insira a segunda nota:"))
+
+media = nota1 * nota2 / 2
+
+if media >= 5:
+    print("aprovado")
+else:
+    print("reprovado")
